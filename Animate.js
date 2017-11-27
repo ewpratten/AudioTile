5bd936ba8312ca252ec373259ec57349152feb53
paddi <paddim8@gmail.com>
2017-11-27 19:23:40 +0100

    Added bubbles!

---
+++ Animate.js
@@ -0,0 +1,24 @@
+function openLandingPage() {
+
+}
+var size = {
+  width: window.innerWidth || document.body.clientWidth,
+  height: window.innerHeight || document.body.clientHeight
+}
+
+window.onload = function() {
+  //resizeWrappers();
+};
+
+window.onresize = function(event) {
+  //resizeWrappers();  
+};
+
+function resizeWrappers() {
+  var slideWrapper1 = document.getElementById("slideWrapper1");
+  var slideWrapper2 = document.getElementById("slideWrapper2");
+
+  var diagonalLength = Math.sqrt(Math.pow(size.width, 2) + Math.pow(size.height, 2));
+  slideWrapper1.style.width = diagonalLength;
+  slideWrapper2.style.width = diagonalLength;
+}
